6144e465f470d5f215e96ca7f01f1062f24c7ca8
HUSTUD <husamelddin@gmail.com>
2025-06-29 12:54:50 +0100

    Fix: Move CSS import to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // You may need to create this file if it doesn't exist
+import './index.css' // This is the correct place for the CSS import
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
